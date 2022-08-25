0984b4f809c71630c046c1a6a4becbe41792d817
Platt <lplatt@usgs.gov>
2022-08-25 12:40:06 -0500

    add the frame PNGs & the associated text json file

---
+++ src/assets/text/timeseriesScrollyText.js
@@ -0,0 +1,31 @@
+import { time } from "vue-analytics";
+
+export default {
+      frames: [
+        {
+          // id is used to source image files with the same naming
+          // and used to assign classes to scrolling text
+          id: 'a',
+          text: 'text a: We can measure the presence of an algal bloom using Phycocyanin fluorescence sensors in the stream. However, it is expensive to install these sensors at every stream station, so we need alternative methods to detect blooms. '
+        },
+        {
+          id: 'b',
+          text: 'text b: So, we can use other characteristics of the stream that change during a bloom as a “proxy” for a bloom. For example, dissolved oxygen '
+        },
+        {
+          id: 'c',
+          text: 'text c: So, we can use other characteristics of the stream that change during a bloom as a “proxy” for a bloom. For example, dissolved oxygen, pH '
+        },
+        {
+          id: 'd',
+          text: 'text d: So, we can use other characteristics of the stream that change during a bloom as a “proxy” for a bloom. For example, dissolved oxygen, pH, or water temperature '
+        },
+        {
+          id: 'e',
+          text: 'text e: The graphs show that around the time of an algal bloom, pH and dissolved oxygen levels rise and then fall while water temperature generally increases. These patterns are especially apparent when you look at the same data but for a year where there was not a bloom during those same days. '
+        },
+        {
+          id: 'f',
+          text: 'text f: When you look at the data for the year where there was not a bloom, phycocyanin, pH, and dissolved oxygen are relatively steady, and temperatures are not just on the rise. '
+        ]
+  };
